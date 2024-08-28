f4af312a4dce700184e2ebb1090a93b8c826f041
mussyman24 <121067118+mussyman24@users.noreply.github.com>
2024-08-28 12:10:37 -0700

    Update Program.java
    Finished program 8

--- Program.java
+++ Program.java
@@ -18,19 +18,16 @@ public class Program8 {
         Scanner numScanner2 = new Scanner(System.in);
         num2 = numScanner2.nextInt();
         
-        Calculator calc = new Calculator(num1, num2);
-        
+        Calculator calc = new Calculator(num1,num2);
+        System.out.println("The sum is: " + calc.getSum());
+        System.out.println("The product is: " + calc.getProduct());
+        System.out.println("The average is: " + calc.getAverage());
+        System.out.println("The Absolute value is: " + calc.getAbsValue());
+        System.out.println("The Maximum Value is: " + calc.getMaxValue());
+        System.out.println("The Minimum Value is: " + calc.getMinValue());
     }
     
 }
-
-
-//Paste console output below:
-/*
-
-
-*/
-
 
 
 
@@ -42,37 +39,72 @@ public class Program8 {
  */
 public class Calculator
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Calculator
-     */
+ 
+    
+    int sum;
+    int product;
+    double average;
+    int absValue;
+    int maxValue;
+    int minValue;
+    
     public Calculator(int num1, int num2)
     {
-        int sum = num1 + num2;
-        int product = num1*num2;
-        double average = sum/2.0;
-        int absValue = Math.abs(num1 - num2);
-        int maxValue = absValue + num1 + num2;
-        int minValue =  Math.abs(num2 - num1) + num1 + num2;
+        sum = num1 + num2;
+        product = num1*num2;
+        average = sum/2.0;
+        absValue = Math.abs(num1 - num2);
+        maxValue = (absValue + num1 + num2)/2;
+        minValue =  (Math.abs(num1 + num2) + absValue)/2;
         
         
         
         
         
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public int getSum() {
+        
+        return sum;
+    }
+    
+    public int getProduct() {
+        
+        return product;
+        
+    }
+    
+    public double getAverage() {
+        
+        return average;
+    }
+    
+    public int getAbsValue() {
+        
+        return absValue;
+    }
+    
+    public int getMaxValue() {
+        
+        return maxValue;
+    }
+    
+    public int getMinValue() {
+        
+        return minValue;
     }
 }
+
+/*
+Enter an integer: 
+12
+Enter another integer: 
+20
+The sum is: 32
+The product is: 240
+The average is: 16.0
+The Absolute value is: 8
+The Maximum Value is: 20
+The Minimum Value is: 20
+*/
 
